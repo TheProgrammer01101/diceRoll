90d5586f42c1eda64d3a89e1fa0cc79d8421ec9f
Barzi Jaf <barzidev@gmail.com>
2023-09-21 15:51:45 +0200

    added soundEffect

--- app.js
+++ app.js
@@ -3,4 +3,6 @@ const img = document.querySelector('img');
 
 button.addEventListener('click', ()=> {
     img.src = `img/${Math.floor(Math.random() * 6) + 1}.svg`;
+    let soundEffect = new Audio('soundEffect.m4a');
+    soundEffect.play();
 })
